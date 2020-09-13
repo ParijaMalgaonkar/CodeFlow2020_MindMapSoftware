022ffda155ef960e111af925ebb267845b8993c8
Nandita M <40920879+nandita-hm@users.noreply.github.com>
2020-09-13 19:43:20 +0530

    Added necessary js files from youtube video

---
+++ demo/src/base.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const app=firebaseConfig.initializeApp({
+    apiKey: "AIzaSyCVMFUelpTDyTLbtvvQekidLeAej5X_1X4",
+    authDomain: "helloworld-94b6e.firebaseapp.com",
+    databaseURL: "https://helloworld-94b6e.firebaseio.com",
+    projectId: "helloworld-94b6e",
+    storageBucket: "helloworld-94b6e.appspot.com",
+    messagingSenderId: "202281983869",
+    appId: "1:202281983869:web:1fc6bcbdc861fb39233614",
+    measurementId: "G-1QLT29JP6Z"
+  });
+
+  export default app;
